716b795c37a145627f656bdf53ba421fbe993ebd
SteveJrong <1554052208@qq.com>
2021-05-15 09:32:13 +0800

    切换开发分支 1.0.1

--- src/main/java/com/stevejrong/android/apk/downloader/provider/app/download/appDownload/impl/AnZhiAppDownload.java
+++ src/main/java/com/stevejrong/android/apk/downloader/provider/app/download/appDownload/impl/AnZhiAppDownload.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright ${YEAR} Steve Jrong
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.stevejrong.android.apk.downloader.provider.app.download.appDownload.impl;
 
 import com.stevejrong.android.apk.downloader.common.Constants;
@@ -30,21 +45,21 @@ public class AnZhiAppDownload extends AbstractAppDownload implements IAppDownloa
     public void automaticDownload() {
         // 浏览器参数初始化
         WebDriver webDriver = this.initBrowserPrefs();
-        // 获取APP队列中的APP列表
-        LinkedList<String> appList = QueueUtil.APP_QUEUE;
+        // APP队列
+        LinkedList<String> appQueue = QueueUtil.APP_QUEUE;
 
-        for (int i = 0; i < appList.size(); i++) {
+        for (int i = 0; i < appQueue.size(); i++) {
             try {
-                webDriver.get(super.searchUrl.replace(Constants.APP_NAME_SYMBOL.val(), appList.element()));
+                webDriver.get(super.searchUrl.replace(Constants.APP_NAME_SYMBOL.val(), appQueue.element()));
                 Thread.sleep(800);
 
                 for (String htmlAnalysisExp : super.htmlAnalysisExpressions) {
-                    webDriver.findElement(By.xpath(htmlAnalysisExp.replace(Constants.APP_NAME_SYMBOL.val(), appList.element()))).click();
+                    webDriver.findElement(By.xpath(htmlAnalysisExp.replace(Constants.APP_NAME_SYMBOL.val(), appQueue.element()))).click();
                 }
 
                 QueueUtil.APP_QUEUE.poll();
             } catch (NoSuchElementException ex) {
-                System.out.println(String.format("%s 未搜索到！", appList.element()));
+                System.out.println(String.format("%s 未搜索到！", appQueue.element()));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- src/main/java/com/stevejrong/android/apk/downloader/provider/app/download/appDownload/impl/TencentAppDownload.java
+++ src/main/java/com/stevejrong/android/apk/downloader/provider/app/download/appDownload/impl/TencentAppDownload.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright ${YEAR} Steve Jrong
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.stevejrong.android.apk.downloader.provider.app.download.appDownload.impl;
 
 import com.stevejrong.android.apk.downloader.common.Constants;
@@ -8,6 +23,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class TencentAppDownload extends AbstractAppDownload implements IAppDownload {
@@ -24,18 +40,18 @@ public class TencentAppDownload extends AbstractAppDownload implements IAppDownl
     public void automaticDownload() {
         // 浏览器参数初始化
         WebDriver webDriver = this.initBrowserPrefs();
-        // 获取APP队列中的APP列表
-        List<String> appList = QueueUtil.APP_QUEUE;
+        // APP队列
+        LinkedList<String> appQueue = QueueUtil.APP_QUEUE;
 
-        for (String app : appList) {
+        for (int i = 0; i < appQueue.size(); i++) {
             try {
-                webDriver.get(super.searchUrl.replace(Constants.APP_NAME_SYMBOL.val(), app));
+                webDriver.get(super.searchUrl.replace(Constants.APP_NAME_SYMBOL.val(), appQueue.element()));
                 Thread.sleep(800);
 
-                for (int i = 0; i < super.htmlAnalysisExpressions.size(); i++) {
-                    webDriver.findElement(By.xpath(htmlAnalysisExpressions.get(i).replace(Constants.APP_NAME_SYMBOL.val(), app))).click();
+                for (int j = 0; j < super.htmlAnalysisExpressions.size(); j++) {
+                    webDriver.findElement(By.xpath(htmlAnalysisExpressions.get(j).replace(Constants.APP_NAME_SYMBOL.val(), appQueue.element()))).click();
 
-                    if (i == 0) {
+                    if (j == 0) {
                         String firstHandle = webDriver.getWindowHandle();
                         for (String winHandle : webDriver.getWindowHandles()) {
                             if (winHandle.equals(firstHandle)) {
@@ -47,7 +63,7 @@ public class TencentAppDownload extends AbstractAppDownload implements IAppDownl
                         }
                     }
 
-                    if (i == 1) {
+                    if (j == 1) {
                         QueueUtil.APP_QUEUE.poll();
 
                         try {
@@ -70,7 +86,7 @@ public class TencentAppDownload extends AbstractAppDownload implements IAppDownl
                     Thread.sleep(800);
                 }
             } catch (NoSuchElementException ex) {
-                System.out.println(String.format("%s 未搜索到！", app));
+                System.out.println(String.format("%s 未搜索到！", appQueue.element()));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
